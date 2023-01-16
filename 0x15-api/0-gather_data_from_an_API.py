b1652d0d5a4a1fbce8f1de09e9943b22ea31a6af
DAVE2456 <davehunegnaw@gmail.com>
2023-01-16 11:04:00 +0300

    initial commit

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,1 +1,35 @@
-0-gather_data_from_an_API.py
+#!/usr/bin/python3
+"""
+Python script that, using this REST API,
+for a given employee ID, returns information about his/her TODO list progress.
+"""
+import requests as r
+from sys import argv
+
+if __name__ == "__main__":
+    employee_id = argv[1]
+    url_user = 'https://jsonplaceholder.typicode.com/users/{}'.format(
+        employee_id)
+    url_todos = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(
+        employee_id)
+
+    resp_user = r.get(url_user)
+    resp_todos = r.get(url_todos)
+
+    try:
+        users = resp_user.json()
+        user_todos = resp_todos.json()
+
+        completed_task = list(
+            filter(lambda x: x.get("completed") is True,
+                   user_todos))
+
+        print("Employee {} is done with tasks({}/{}):"
+              .format(users.get('name'), len(completed_task),
+                      len(user_todos)))
+
+        for todo in completed_task:
+            print("\t {}".format(todo.get('title')))
+
+    except Exception as e:
+        print(e)
