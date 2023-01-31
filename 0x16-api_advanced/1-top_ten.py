65d6dd8346ac5ebe6da84b0387c01074fc62d52c
DAVE2456 <davehunegnaw@gmail.com>
2023-01-31 11:39:53 +0300

    initial commit

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Function the queries the Reddit API and 
+prints the titles of the first 10 hot pists listed
+for a given subreddit
+"""
+
+import requests
+
+
+def top_ten(subreddit):
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    params = {
+        'limit': 10
+    }
+    req = requests.get(
+        url, headers={'User-Agent': 'Python/requests'}, params=params)
+
+    try:
+        resp = req.json()
+        posts= = respo.get("data", {}).get("children", None)
+        if posts is None:
+            print(None)
+        else:
+            [print(post.get("data").get("title")) for post in posts]
+    except Exception:
+        print(None)
+    
