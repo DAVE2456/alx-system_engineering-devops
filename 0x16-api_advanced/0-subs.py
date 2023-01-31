707ec4b4b2a920b75f5d8f0648897061135e7744
DAVE2456 <davehunegnaw@gmail.com>
2023-01-31 11:19:47 +0300

    initial commit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python3
 """
-0-main
-"""
-import sys
 
-if __name__ == '__main__':
-    number_of_subscribers = __import__('0-subs').number_of_subscribers
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        print("{:d}".format(number_of_subscribers(sys.argv[1])))
+Function the queris the Reddit API and
+return the number of subscribers
+for a given subreddit 
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    req = requests.get(url, headers={'User-Agent': 'Python/requests})
+
+    try:
+       resp = req.json()
+       return resp.get("data", {}).get("subscribers", 0)
+except Exception:
+       return 0
